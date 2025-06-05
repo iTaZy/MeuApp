4bffe1707c313b9f9b21fbcbc26777d5cd16abb6
User <luiz_felipe_2002@yahoo.com>
2025-06-05 13:05:35 -0300

    Matches v1

--- app/src/main/java/com/tazy/meuapp/conexoes/TelaConexoes.kt
+++ app/src/main/java/com/tazy/meuapp/conexoes/TelaConexoes.kt
@@ -15,8 +15,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
